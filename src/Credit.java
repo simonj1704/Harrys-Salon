291a70080dbe13a3913962adaa482cc6edc8eaf6
simonj1704 <simo915m@stud.kea.dk>
2022-10-04 13:42:01 +0200

    Added hasCredit in Credit Class

--- src/Credit.java
+++ src/Credit.java
@@ -1,2 +1,11 @@
 public class Credit {
+    private boolean hasCredit;
+
+    public boolean isHasCredit() {
+        return hasCredit;
+    }
+
+    public void setHasCredit(boolean hasCredit) {
+        this.hasCredit = hasCredit;
+    }
 }
