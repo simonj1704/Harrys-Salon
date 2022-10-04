00dd4e520e7c2e8fd838655a5fcb647ea23b198d
simonj1704 <simo915m@stud.kea.dk>
2022-10-04 13:43:32 +0200

    Added Getters and setters in Product Class

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,12 @@
 public class Product {
     String[] products;
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+
 }
