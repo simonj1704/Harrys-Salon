29f9a0b806e8ac2c4b013c3b1258e96bce419268
simonj1704 <simo915m@stud.kea.dk>
2022-10-05 10:01:14 +0200

    Wrote ProductList and Product

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
 
     public static void run() {
         Menu menu1 = new Menu();
+        ProductList productList = new ProductList();
+        System.out.println(productList.getProductList());
     }
 }
