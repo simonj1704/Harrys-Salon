e3d46b4abaa0652c5ad585c080fe9d3e7628bb12
Anders <113690119+acb100@users.noreply.github.com>
2022-10-05 10:34:26 +0200

    Sat menu ud i run

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ public class Main {
 
     void run(){
         System.out.println("Hello world!");
+        Menu menu = new Menu("Velkommen til Harrys Salon bookingapplikation", "Vælg input",
+                new String[]{"1 . Book tid"});
+
+        boolean inputIsInt;
+        do {
+            menu.printMenu();
+            switch (menu.readInput()) {
+                case 1:
+                    System.out.println("Du har valgt 1.");
+                    inputIsInt = true;
+                    break;
+                default:
+                    System.out.println("Indtast venligst et tal fra menupunktet.");
+                    inputIsInt = false;
+                    break;
+            }
+        } while (!inputIsInt);
     }
     public static void main(String[] args) {
          new Main().run();
