58c3041ce8e7e27f04a184dd6232596b0011f33a
Hoejhus <mikk57k0@stud.kea.dk>
2022-10-05 09:37:06 +0200

    Update product

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run();
+    }
+
+    public static void run() {
+        Menu menu1 = new Menu();
     }
 }
