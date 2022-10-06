94044db5b5aabd0e2c8386b52926a855ca9e4af4
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2022-10-06 13:00:41 +0200

    Fixed menu method

--- src/Sale.java
+++ src/Sale.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Sale {
+
+    Scanner in = new Scanner(System.in);
     private double haircutPrice =  200;
     private int numberOfProducts = 0;
     private double productPrice;
@@ -6,6 +10,7 @@ public class Sale {
     private double totalPrice;
     private String product;
     private String productName;
+    boolean askAgain = true;
     ProductList products = new ProductList();
     Customer customer = new Customer();
     DateList dateList = new DateList();
@@ -17,10 +22,12 @@ public class Sale {
 
     public void addProduct(){
         Product product;
-
         product = products.productList.get(1);
         productPrice = product.getPrice();
         productName = product.getProduct();
+
+        System.out.println(products);
+        System.out.println("Which item do you want to add?");
     }
 
     public boolean checkCredit(){
@@ -32,14 +39,26 @@ public class Sale {
     
     public void checkSale(){
 
-
     }
-    public void addSale(String input){
-        date = dateList.checkDate(input);
-        addProduct();
+    public void addSale(String inputDate, int time) {
+        String addSale = in.nextLine();
+
         totalPrice = haircutPrice + productPrice;
         Sale sale = new Sale(totalPrice, productName);
-        dateList.date.timeSlot.sales.add(date, sale);
+        dateList.date.timeSlot.sales.add(time, sale);
+        date = dateList.checkDate(inputDate);
+        Date date1;
+        date1 = dateList.dates.set(date, dateList.dates.get(date));
 
+
+        System.out.println("Do you want to add a product? yes/no");
+
+        if (addSale.equals("yes")) {
+            addProduct();
+            date1.timeSlot.sales.add(time, sale);
+        } else if (addSale.equals("no"))
+            date1.timeSlot.sales.add(time, sale);
+        else
+            System.out.println("Type yes or no");
     }
 }
